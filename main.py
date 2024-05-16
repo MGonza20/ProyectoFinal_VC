b914a050add5e31d5fc92fdf0995f91bafeefcdf
Guillermo Santos <guillermo.santosba@gmail.com>
2024-05-16 17:42:25 -0600

    Added basic structure for the analyzer

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
+from src.video_processor import process_video
 
 
 if __name__ == '__main__':
 	# 1. Open Video To Process
 	match_name = 'match01'
-
 	# 2. Process the video into frames
+	process_video(match_name)
+	print('processed')
---
+++ src/video_processor.py
@@ -0,0 +1,37 @@
+import cv2
+
+def process_video(match_name='match01', ball_detector=None,court_detector=None,people_detector=None):
+	'''
+	Process the video of a match and return the processed frames
+	Parameters:
+		match_name: The name of the match to process
+		ball_detector: The BallDetector object to use
+		court_detector: The CourtDetector object to use
+		people_detector: The PeopleDetector object to use
+	'''
+	# 1. Open Video To Process
+	video_path = f'data/raw/videos/{match_name}.mp4'
+	output_video_path = f'output/videos/{match_name}.mp4'
+	cap = cv2.VideoCapture(video_path)
+	# Out Video Configuration
+	fps = int(cap.get(cv2.CAP_PROP_FPS))
+	frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+	frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
+
+	# 2. Process the video into frames
+	while True:
+		ret, frame = cap.read()
+		if not ret:
+			break
+		# 3. Process the frame
+		# 4. Detect the ball
+		# 5. Detect the court
+		# 6. Detect the people
+		# 7. Out the frame
+		out.write(frame)
+
+	cap.release()
+	out.release()
+	cv2.destroyAllWindows()
