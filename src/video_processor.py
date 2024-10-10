bba3543bc3d6d9ef841125835d7df713a5ee145a
Guillermo Santos <guillermo.santosba@gmail.com>
2024-10-09 20:12:02 -0600

    progress

--- src/video_processor.py
+++ src/video_processor.py
@@ -25,13 +25,13 @@ def process_video(match_name='match01', ball_detector=None,court_detector=None,p
 	frames = read_frames(cap) 
 
 	# Detect ball
-	ball_detections = ball_detector.detect_frames(frames,output_path=f'output/stubs/balls-{match_name}.pkl')
+	ball_detections = ball_detector.detect_frames(frames,stub_path=f'output/stubs/balls-{match_name}.pkl')
 	ball_detections = ball_detector.interpolate(ball_detections)
 	# Detect the court
 	if court_detector is not None:
-		court_lines = court_detector.detect_frames(frames,output_path=f'output/stubs/court-{match_name}.pkl')
+		court_lines = court_detector.detect_frames(frames,stub_path=f'output/stubs/court-{match_name}.pkl')
 		# Detect the players
-	player_detections = people_detector.detect_frames(frames,output_path=f'output/stubs/players-{match_name}.pkl')
+	player_detections = people_detector.detect_frames(frames,stub_path=f'output/stubs/players-{match_name}.pkl')
 	# player_detections = people_detector.filter_players(player_detections, court_lines)
 
 	# Court drawing
